d35f82d28320292a7ed45cb8d88caf7ed31ee50c
run-after <run.after@protonmail.com>
2020-01-27 17:20:11 -0600

    add all moves for Queen. Will need to come back when check is established

--- queen.rb
+++ queen.rb
@@ -1,17 +1,150 @@
-class Queen
-  attr_accessor :position
-  def initialize(position)
+require "./piece.rb"
+
+class Queen < Piece
+
+  def move1(position)
     @position = position
+    @moves = []
+  end
+
+  def next_move(position, board)
+    @position = position
+    @moves = []
+    position_column = [nil, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    column = position[0]
+    row = position[1].to_i
+### diagonals ###
+    i = 1
+    while i < 8
+      up_right = position_column[position_column.find_index(column)+i].to_s + (row + i).to_s
+      if empty_square?(up_right, board)
+        @moves << up_right 
+        i+=1
+      elsif enemy_square?(up_right, board)
+        @moves << up_right
+        i = 8
+      else
+        i = 8
+      end
+    end
+    
+    i = 1
+    while i < 8
+      down_right = position_column[position_column.find_index(column)+i].to_s + (row - i).to_s
+      if empty_square?(down_right, board)
+        @moves << down_right
+        i+=1
+      elsif enemy_square?(down_right, board)
+        @moves << down_right
+        i = 8
+      else
+        i = 8
+      end
+    end
+
+    i = 1
+    while i < 8
+      up_left = position_column[position_column.find_index(column)-i].to_s + (row + i).to_s
+      if empty_square?(up_left, board)
+        @moves << up_left
+        i+=1
+      elsif enemy_square?(up_left, board)
+        @moves << up_left
+        i = 8
+      else
+        i = 8
+      end
+    end
+    
+    i = 1
+    while i < 8
+      down_left = position_column[position_column.find_index(column)-i].to_s + (row - i).to_s
+      if empty_square?(down_left, board)
+        @moves << down_left 
+        i+=1
+      elsif enemy_square?(down_left, board)
+        @moves << down_left
+        i = 8
+      else
+        i = 8
+      end
+    end
+### straight moves ###    
+    i = 1
+    while i < 8
+      right = position_column[position_column.find_index(column) + i].to_s + row.to_s
+      if empty_square?(right, board)
+        @moves << right
+        i+=1
+      elsif enemy_square?(right, board)
+        @moves << right
+        i = 8
+      else
+        i = 8
+      end
+    end
+
+    i = 1
+    while i < 8
+      left = position_column[position_column.find_index(column) - i].to_s + row.to_s
+      if empty_square?(left, board)
+        @moves << left
+        i+=1
+      elsif enemy_square?(left, board)
+        @moves << left
+        i = 8
+      else
+        i = 8
+      end
+    end
+
+    i = 1
+    while i < 8
+      up = column + (row + i).to_s
+      if empty_square?(up, board)
+        @moves << up
+        i+=1
+      elsif enemy_square?(up, board)
+        @moves << up
+        i = 8
+      else
+        i = 8
+      end
+    end
+
+    i = 1
+    while i < 8
+      down = column + (row - i).to_s
+      if empty_square?(down, board)
+        @moves << down
+        i+=1
+      elsif enemy_square?(down, board)
+        @moves << down
+        i = 8
+      else
+        i = 8
+      end
+    end
+
   end
 end
-
 class WhiteQueen < Queen
+
+  def team
+    "white"
+  end
+
   def show
     "[♛]"
   end
 end
 
 class BlackQueen < Queen
+
+  def team
+    "black"
+  end
+  
   def show
     "[♕]"
   end
