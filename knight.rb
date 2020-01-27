34d03923595b98e829588c51ef8db2929d98af49
run-after <run.after@protonmail.com>
2020-01-27 16:48:16 -0600

    all moves complete. Will revisit when check is established

--- knight.rb
+++ knight.rb
@@ -1,17 +1,76 @@
-class Knight
-  attr_accessor :position
-  def initialize(position)
+require "./piece.rb"
+
+class Knight < Piece
+
+  def move1(position)
     @position = position
+    @moves = []
+    position_column = [nil, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    column = position[0]
+    row = position[1].to_i
+    upper_right = position_column[position_column.find_index(column)+1].to_s + (row + 2).to_s
+    @moves << upper_right 
+    upper_left = position_column[position_column.find_index(column)-1].to_s + (row + 2).to_s
+    @moves << upper_left 
+    lower_right = position_column[position_column.find_index(column)+1].to_s + (row - 2).to_s
+    @moves << lower_right 
+    lower_left = position_column[position_column.find_index(column)-1].to_s + (row - 2).to_s
+    @moves << lower_left 
   end
+
+  def next_move(position, board)
+    @position = position
+    @moves = []
+    
+    position_column = [nil, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    column = position[0]
+    row = position[1].to_i
+  
+    upper_right = position_column[position_column.find_index(column)+1].to_s + (row + 2).to_s
+    @moves << upper_right if empty_square?(upper_right, board) || enemy_square?(upper_right, board)
+
+    righter_up = position_column[position_column.find_index(column) + 2].to_s + (row + 1).to_s
+    @moves << righter_up if empty_square?(righter_up, board) || enemy_square?(righter_up, board)
+
+    righter_low = position_column[position_column.find_index(column)+2].to_s + (row - 1).to_s
+    @moves << righter_low if empty_square?(righter_low, board) || enemy_square?(righter_low, board)
+
+    lower_right = position_column[position_column.find_index(column)+1].to_s + (row - 2).to_s
+    @moves << lower_right if empty_square?(lower_right, board) || enemy_square?(lower_right, board)
+
+    upper_left = position_column[position_column.find_index(column)-1].to_s + (row + 2).to_s
+    @moves << upper_left if empty_square?(upper_left, board) || enemy_square?(upper_left, board)
+
+    lefter_up = position_column[position_column.find_index(column)-2].to_s + (row + 1).to_s
+    @moves << lefter_up if empty_square?(lefter_up, board) || enemy_square?(lefter_up, board)
+
+    lefter_low = position_column[position_column.find_index(column)-2].to_s + (row - 1).to_s
+    @moves << lefter_low if empty_square?(lefter_low, board) || enemy_square?(lefter_low, board)
+
+    lower_left = position_column[position_column.find_index(column)-1].to_s + (row - 2).to_s
+    @moves << lower_left if empty_square?(lower_left, board) || enemy_square?(lower_left, board)
+
+  end
+
 end
 
 class WhiteKnight < Knight
+  
+  def team
+    "white"
+  end
+  
   def show
     "[♞]"
   end
 end
 
 class BlackKnight < Knight
+
+  def team
+    "black"
+  end
+
   def show
     "[♘]"
   end
