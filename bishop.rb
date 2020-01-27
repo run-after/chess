ff0bab8dbce6cb9bac1c8c7e79be0b90015057ac
run-after <run.after@protonmail.com>
2020-01-27 16:30:07 -0600

    remove some repeated code

--- bishop.rb
+++ bishop.rb
@@ -1,13 +1,11 @@
 require "./piece.rb"
 require "pry"
-class WhiteBishop < Piece
+class Bishop < Piece
   def move1(position)
-    @team = "white"
     @position = position
     @moves = []
   end
   def next_move(position, board)
-    @team = "white"
     @position = position
     @moves = []
     position_column = [nil, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -70,83 +68,22 @@ class WhiteBishop < Piece
       end
     end
   end
+end
 
+class WhiteBishop < Bishop
+  
+  def team
+    "white"
+  end
   def show
     "[♝]"
   end
 end
 
-class BlackBishop < Piece
-
-  def move1(position)
-    @team = "black"
-    @position = position
-    @moves = []
-  end
-
-  def next_move(position, board)
-    @team = "black"
-    @position = position
-    @moves = []
-    position_column = [nil, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    column = position[0]
-    row = position[1].to_i
-    ### up and to the right ###
-    i = 1
-    while i < 8
-      diagonal_attack = position_column[position_column.find_index(column)+i].to_s + (row + i).to_s
-      if empty_square?(diagonal_attack, board)
-        @moves << diagonal_attack 
-        i+=1
-      elsif enemy_square?(diagonal_attack, board)
-        @moves << diagonal_attack
-        i = 8
-      else
-        i = 8
-      end
-    end
-    ### down and to the right ###
-    i = 1
-    while i < 8
-      diagonal_attack = position_column[position_column.find_index(column)+i].to_s + (row - i).to_s
-      if empty_square?(diagonal_attack, board)
-        @moves << diagonal_attack 
-        i+=1
-      elsif enemy_square?(diagonal_attack, board)
-        @moves << diagonal_attack
-        i = 8
-      else
-        i = 8
-      end
-    end
-    ### up and to the left attack ###
-    i = 1
-    while i < 8
-      diagonal_attack = position_column[position_column.find_index(column)-i].to_s + (row + i).to_s
-      if empty_square?(diagonal_attack, board)
-        @moves << diagonal_attack 
-        i+=1
-      elsif enemy_square?(diagonal_attack, board)
-        @moves << diagonal_attack
-        i = 8
-      else
-        i = 8
-      end
-    end
-    ### down and to the left ###
-    i = 1
-    while i < 8
-      diagonal_attack = position_column[position_column.find_index(column)-i].to_s + (row - i).to_s
-      if empty_square?(diagonal_attack, board)
-        @moves << diagonal_attack 
-        i+=1
-      elsif enemy_square?(diagonal_attack, board)
-        @moves << diagonal_attack
-        i = 8
-      else
-        i = 8
-      end
-    end
+class BlackBishop < Bishop
+  
+  def team
+    "black"
   end
   def show
     "[♗]"
