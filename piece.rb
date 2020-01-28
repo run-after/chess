959bdd5c615d964be047dd2d0156e27987d6c8f8
run-after <run.after@protonmail.com>
2020-01-27 19:52:36 -0600

    fix the #enemy_square? method... Was not allowing black to kill enemy. Now good

--- piece.rb
+++ piece.rb
@@ -2,7 +2,7 @@ class Piece
   attr_accessor :position, :moves, :team, :OG_position, :first_move
     def initialize(position, round)
       @OG_position = position
-      @round = round
+      #@round = round
       ### needed for pawn ###
       if round == 1 || round == 2
         move1(position)
@@ -12,9 +12,7 @@ class Piece
     end
 
     def enemy_square?(move, board)
-      team = "black" if @round % 2 != 0
-      team = "white" if @round % 2 == 0
-      board[move.to_sym] != nil && board[move.to_sym].team == team
+      board[move.to_sym] != nil && board[move.to_sym].team != self.team
     end
   
     def empty_square?(move, board)
